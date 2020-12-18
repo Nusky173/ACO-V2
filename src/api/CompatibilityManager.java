7e20cf3a1189a39c664f9fe6850824dbc5530033
simon <simon.lecordier@etudiant.univ-rennes1.fr>
2020-12-18 17:33:32 +0100

    test compatibilityManager + modif remove manager

--- src/api/CompatibilityManager.java
+++ src/api/CompatibilityManager.java
@@ -2,6 +2,7 @@ package api;
 
 import java.util.Set;
 
+import exceptions.ConfigurationException;
 import exceptions.InvalidParameterException;
 
 public interface CompatibilityManager extends CompatibilityChecker {
@@ -10,13 +11,13 @@ public interface CompatibilityManager extends CompatibilityChecker {
     void addIncompatibilities(PartType reference, Set<PartType> target)  throws InvalidParameterException;
     
     /* Supprime une incompatiblité a  une partie de référence */
-    void removeIncompatibility(PartType reference, PartType target) throws InvalidParameterException ;
+    void removeIncompatibility(PartType reference, PartType target) throws ConfigurationException ;
     
     /*Ajoute des parties n�cessaire a� la partie de r�f�rence.*/
     void addRequirements(PartType reference, Set<PartType> target) throws InvalidParameterException ;
     
     /*Retire une partie qui été nécessaire à la partie de référence.*/
-    void removeRequirement(PartType reference, PartType target)  throws InvalidParameterException;
+    void removeRequirement(PartType reference, PartType target)  throws ConfigurationException;
 
 
 }
--- src/impl/Session.java
+++ src/impl/Session.java
@@ -1,6 +1,11 @@
 package impl;
 
+import java.util.HashSet;
+import java.util.Set;
+import java.util.logging.Logger;
+
 import api.CompatibilityManager;
+import api.PartType;
 import enums.CategoryType;
 import enums.RoleType;
 import exceptions.InvalidParameterException;
@@ -9,7 +14,7 @@ import parts.*;
 public class Session 
 {
 	public static Session INSTANCE = new Session();
-	
+	private static Logger logger = Logger.getGlobal();
 	public ConfigurationImpl configuration;
 	public CompatibilityManager compatibilityManager;
 	public ConfiguratorImpl configurator;
@@ -22,10 +27,19 @@ public class Session
 		this.configuration = new ConfigurationImpl();
 		this.compatibilityManager = (CompatibilityManager)configuration.getConfigurator().getCompatibilityChecker();
 		this.configurator = ((ConfiguratorImpl)configuration.getConfigurator());
-		this.initialize();
+		
+		try 
+		{
+			this.initialize();
+		} 
+		catch (InvalidParameterException e) 
+		{
+			e.printStackTrace();
+		}
+		
 	}
 	
-	private void initialize()
+	private void initialize() throws InvalidParameterException
 	{
 		configurator.add(EG100.class, CategoryType.Engine);
 		configurator.add(EG133.class, CategoryType.Engine);
@@ -48,6 +62,50 @@ public class Session
 		configurator.add(IN.class, CategoryType.Interior);
 		configurator.add(IH.class, CategoryType.Interior);
 		configurator.add(IS.class, CategoryType.Interior);
+		
+		//incompatibilities
+		Set<PartType> incompTA5 = new HashSet<PartType>();
+		Set<PartType> incompTSF7 = new HashSet<PartType>();
+		Set<PartType> incompXC = new HashSet<PartType>();
+		Set<PartType> incompXM = new HashSet<PartType>();
+		Set<PartType> incompXS = new HashSet<PartType>();
+		Set<PartType> incompIS = new HashSet<PartType>();
+		//requirements
+		Set<PartType> requirementsEH120 = new HashSet<PartType>();
+		Set<PartType> requirementsTC120 = new HashSet<PartType>();
+		Set<PartType> requirementsXS = new HashSet<PartType>();
+		Set<PartType> requirementsIS = new HashSet<PartType>();
+		
+		
+		incompTA5.add(configurator.getPartType("EG100").get());
+		incompTSF7.add(configurator.getPartType("EG100").get());
+		incompTSF7.add(configurator.getPartType("EG133").get());
+		incompTSF7.add(configurator.getPartType("ED110").get());
+		incompXC.add(configurator.getPartType("EG210").get());
+		incompXM.add(configurator.getPartType("EG210").get());
+		incompXS.add(configurator.getPartType("EG100").get());
+		incompIS.add(configurator.getPartType("EG100").get());
+		incompIS.add(configurator.getPartType("TM5").get());
+		
+		requirementsEH120.add(configurator.getPartType("TC120").get());
+		requirementsTC120.add(configurator.getPartType("EH120").get());
+		requirementsXS.add(configurator.getPartType("IS").get());
+		requirementsIS.add(configurator.getPartType("XS").get());
+		
+		
+		//ajouter à compatibilityManager
+		compatibilityManager.addIncompatibilities(configurator.getPartType("TA5").get(), incompTA5);
+		compatibilityManager.addIncompatibilities(configurator.getPartType("TSF7").get(), incompTSF7);
+		compatibilityManager.addIncompatibilities(configurator.getPartType("XC").get(), incompXC);
+		compatibilityManager.addIncompatibilities(configurator.getPartType("XM").get(), incompXM);
+		compatibilityManager.addIncompatibilities(configurator.getPartType("XS").get(), incompXS);
+		compatibilityManager.addIncompatibilities(configurator.getPartType("IS").get(), incompIS);
+		
+		compatibilityManager.addRequirements(configurator.getPartType("IS").get(), requirementsIS);
+		compatibilityManager.addRequirements(configurator.getPartType("XS").get(), requirementsXS);
+		compatibilityManager.addRequirements(configurator.getPartType("TC120").get(), requirementsTC120);
+		compatibilityManager.addRequirements(configurator.getPartType("EH120").get(), requirementsEH120);
+		
 		
 	}
 	
